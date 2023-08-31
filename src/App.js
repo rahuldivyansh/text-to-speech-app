2bb681b8709ac22e2ec0a457e58af535638a61c1
rahuldivyansh <90146156+rahuldivyansh@users.noreply.github.com>
2023-09-01 00:42:52 +0530

    Made some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [textToCopy, setTextToCopy] = useState();
+  const [isCopied, setCopied] = useClipboard(textToCopy, {
+    successDuration: 1000,
+  });
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+    const stopListening = () => SpeechRecognition.stopListening();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser does not support speech recognition.</span>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="container">
+        <h2>Speech to Text Converter</h2>
+        <br />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Click on Start Listening and speak what you want.
+          <br />
+          <br />
+          You can also copy the text to clipboard by clicking on Copy to clipboard.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+          {transcript}
+        </div>
+
+        <div className="btn-style">
+          <button onClick={setCopied}>
+            {isCopied ? "Text Copied" : "Click to Copy"}
+          </button>
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={stopListening}>
+            Stop Listening
+          </button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
